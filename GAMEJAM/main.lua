13a9f815775a111c96d3945d1dd75e5224c52132
Aluque6 <87724042+Aluque6@users.noreply.github.com>
2025-07-31 00:15:32 +0200

    Update main.lua

--- GAMEJAM/main.lua
+++ GAMEJAM/main.lua
@@ -1,6 +1,3 @@
-
-
-
 function love.load()
   
   w0, h0 = love.graphics.getDimensions()
@@ -15,14 +12,13 @@ function love.load()
   
   velocidad = 500
   
-  
 end
 
 
 function love.update(dt)
 
 moverPerro(dt)
-print(perro.y)
+print(perro.x)
 
 end
 
@@ -35,8 +31,6 @@ function love.draw()
   love.graphics.setColor(1,1,1,1)
   
 end
-
-
 
 function moverPerro(dt)
   
@@ -66,15 +60,13 @@ if love.keyboard.isDown("d") then
   perro.ancho = perro.ancho - 1 * velocidad * dt
 end
  perro.x = perro.x + 1 * velocidad * dt
-if perro.x > w0 - perro.ancho then
-  perro.x = w0 - perro.ancho
+if perro.x > w0 + perro.ancho then
+  perro.x = w0 + perro.ancho
 end
  
 if perro.x > h0 + perro.ancho then
   perro.x = h0 + perro.ancho
 end
-
-
 
 end
 
