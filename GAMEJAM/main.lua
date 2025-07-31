f7ca5d929b3756aab4b1cac812b5b4a515e39e16
Aluque6 <87724042+Aluque6@users.noreply.github.com>
2025-07-31 03:02:31 +0200

    Update main.lua

--- GAMEJAM/main.lua
+++ GAMEJAM/main.lua
@@ -1,76 +1,193 @@
+local currentDirection = nil
+mover = false
+
 function love.load()
+  
   
   w0, h0 = love.graphics.getDimensions()
   
   print(w0, h0)
   
+  scrollX = 0
+  scrollVelocidad = 150
+  
   perro = {}
-  perro.ancho = 100
-  perro.alto = 50
-  perro.x = w0 - perro.ancho
-  perro.y = h0 / 2
+
+  tamañoSegmento = 50
+  tamañoMinimo = 100
+  historialDirecciones = {}
+  
+   xInicial = w0 - tamañoSegmento
+   yInicial = h0 / 2
+  
+  for i = 0, tamañoMinimo - 1 do
+    table.insert(perro, 1, {
+        x = xInicial,
+        y = yInicial})
+  end
+    
+  
+  
+  obstaculosSprites = {}
+  obstaculosSprites.arbol = love.graphics.newImage("sprites/arbol.png")
+  
+  obstaculosMaximos = 10
+  obstaculosCargados = false
+
+  obstaculos = {["arbol"] = 0 , ["caja"] = 0}
+  
+  coordObstaculos = {}
+  
+  posObstaculoY = 100
+  posObstaculoX =  100 
+  
   
   velocidad = 500
   
+  cargarObstaculos(obstaculos)
+  print(obstaculos["arbol"], obstaculos["caja"])
+
 end
 
+tiempoAcumulado = tiempoAcumulado or 0
+intervaloMovimiento = 0.1
 
 function love.update(dt)
+  
+  scrollX = scrollX + scrollVelocidad * dt
 
-moverPerro(dt)
-print(perro.x)
+if currentDirection then
+  tiempoAcumulado = tiempoAcumulado + dt
+  if tiempoAcumulado >= intervaloMovimiento then
+  moverPerroBien()
+  tiempoAcumulado = 0
+  end
+end
+
+print(scrollX)
+
 
 end
+
 
 
 
 function love.draw()
-  
-  love.graphics.setColor(1,0,0,1)
-  love.graphics.rectangle("fill", perro.x, perro.y, perro.ancho, perro.alto)
-  love.graphics.setColor(1,1,1,1)
-  
-end
 
-function moverPerro(dt)
   
-  
-if love.keyboard.isDown("w") then
-  perro.y = perro.y - 1 * velocidad * dt
-  if perro.y < 0  then
-    perro.y = 0
+  love.graphics.setColor(0,1,0)
+  for i, segmento in ipairs(perro) do
+    love.graphics.rectangle("fill", segmento.x + scrollX, segmento.y, tamañoSegmento, tamañoSegmento)
   end
-end
-if love.keyboard.isDown("s") then
-  perro.y = perro.y + 1 * velocidad * dt
-  if perro.y > h0 - perro.alto  then
-    perro.y = h0 - perro.alto
-  end
-end
-if love.keyboard.isDown("a") then
-  perro.ancho = perro.ancho + 1 * velocidad * dt
-  perro.x = perro.x - 1 * velocidad * dt
-  if perro.x < h0/4 then
-  perro.x = h0/4
-end
-end
-
-if love.keyboard.isDown("d") then
-  if perro.ancho > 100 then
-  perro.ancho = perro.ancho - 1 * velocidad * dt
-end
- perro.x = perro.x + 1 * velocidad * dt
-if perro.x > w0 + perro.ancho then
-  perro.x = w0 + perro.ancho
-end
- 
-if perro.x > h0 + perro.ancho then
-  perro.x = h0 + perro.ancho
-end
-
-end
-
-
+  love.graphics.setColor(1,1,1)
+  
+  
+  
   
 end
 
+function love.keypressed(key)
+  
+if key == "w" or key == "a" or key == "s" or key == "d" then
+  currentDirection = key
+  mover = true
+end
+
+end
+
+function love.keyreleased(key)
+  
+  if key == currentDirection then
+    currentDirection = nil
+  end
+  
+end
+
+
+
+function cargarObstaculos(obstaculos)
+  
+  if obstaculosCargados == false then
+    number = love.math.random() * 10
+    obstaculosActuales = math.floor(number)
+    obstaculosCargados = true
+  end
+  for tipo, cantidad in pairs(obstaculos) do
+    cantidad = math.floor(love.math.random() * 10)
+    obstaculos[tipo] =  cantidad
+    print(tipo, cantidad)
+  end
+
+end
+
+function moverObstaculos(obstaculos, dt)
+  
+for tipo, cantidad in pairs(obstaculos) do
+  
+  
+  
+  
+end
+
+
+end
+
+
+function moverPerroBien(dt)
+  
+  local cabeza = perro[#perro]
+  local nuevaX, nuevaY = cabeza.x, cabeza.y
+  local ultimaDireccion = historialDirecciones[#historialDirecciones]
+  
+  if sonDireccionesOpuestas(currentDirection, ultimaDireccion) then
+    if #perro > tamañoMinimo then
+      table.remove(perro, #perro)
+      table.remove(historialDirecciones, #historialDirecciones)
+    end
+    return
+  end
+  
+  if currentDirection == "w" then
+    nuevaY = nuevaY - tamañoSegmento
+  elseif currentDirection == "s" then
+    nuevaY = nuevaY + tamañoSegmento
+  elseif currentDirection == "a" then
+    nuevaX = cabeza.x - tamañoSegmento
+    local posicionEnPanralla = nuevaX + scrollX
+    if posicionEnPanralla < w0 / 4 then
+      return
+    end
+    
+  elseif currentDirection == "d" then
+  if #perro > tamañoMinimo then
+    table.remove(perro, #perro)
+  end
+  return
+else
+  return
+end
+
+
+
+if nuevaY < 0 then 
+  nuevaY = 0 
+end
+if nuevaY > h0 - tamañoSegmento then 
+  nuevaY = h0 - tamañoSegmento 
+end
+
+  if nuevaX ~= cabeza.x or nuevaY ~= cabeza.y then
+    table.insert(perro, {x = nuevaX, y = nuevaY})
+    table.insert(historialDirecciones, currentDirection)
+  end
+  
+  
+  
+end
+
+
+function sonDireccionesOpuestas (dir1, dir2)
+  
+  return (dir1 == "w" and dir2 == "s") or (dir1 == "s" and dir2 == "w") or (dir1 == "a" and dir2 == "d") or (dir1 == "d" and dir2 == "a")
+  
+end
